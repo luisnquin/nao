98a93f14d74ff64169dc60cb572b61bcbaebb151
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-27 22:05:28 -0500

    feat: browser view(header)

---
+++ src/web/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header />
+
+</template>
+
+<style>
+* {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  padding: 0;
+  margin: 0;
+}
+</style>
