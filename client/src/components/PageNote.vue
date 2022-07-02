5757f6faf4359b4ff57e6ffe04d067b3134f08d3
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-07-01 22:30:39 -0500

    feat: indent-textarea pkg + refactorings

--- client/src/components/PageNote.vue
+++ client/src/components/PageNote.vue
@@ -1,56 +1,30 @@
 <script>
+import * as indentation from 'indent-textarea'
+
 export default {
 	name: 'PageNote',
 	data() {
 		return {
-			previousWasTab: false
 		}
 	},
 	mounted: () => {
-		const annotations = document.getElementsByTagName('textarea')
-		for (let i = 0; i < annotations.length; i++) {
-			annotations[i].setAttribute('style', 'height:' + annotations[i].scrollHeight + 'px;overflow-y:hidden;')
-			annotations[i].oninput = function () {
+		const annotations = document.querySelectorAll('.annotation')
+		annotations.forEach((a) => {
+			a.setAttribute('style', 'height:' + a.scrollHeight + 'px;overflow-y:hidden;')
+			a.oninput = function () {
 				this.style.height = 'auto'
 				this.style.height = this.scrollHeight + 'px'
 			}
-			annotations[i].onkeydown = function (e) {
-				if (e.key == 'Tab') {
-					e.preventDefault()
+		})
 
-					const start = this.selectionStart
-
-					this.value = this.value.substring(0, start) + '\t' + this.value.substring(this.selectionEnd)
-					this.selectionStart = this.selectionEnd = start + 1
-
-					this.previousWasTab = true
-
-					return
-				}
-
-				if ((e.key == 'z' || e.key == 'Z') && e.ctrlKey && this.previousWasTab) {
-					e.preventDefault()
-
-					const start = this.selectionStart
-
-					this.value =
-						this.value.substring(0, start).replace('\t', '') + this.value.substring(this.selectionEnd)
-
-					return
-				}
-
-				if (!e.ctrlKey) {
-					this.previousWasTab = false
-				}
-			}
-		}
+		indentation.watch(annotations)
 	}
 }
 </script>
 
 <template>
-	<article class="note">
-		<textarea spellcheck="false">
+	<article class="annotation-container">
+		<textarea class="annotation" spellcheck="false" placeholder="Write something here...">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus ullamcorper leo, ac condimentum magna vestibulum vitae. Vestibulum consectetur risus at enim volutpat, id sodales neque iaculis. Aenean nunc enim, cursus at nisi quis, ultricies blandit mauris. Maecenas mollis molestie pulvinar. Aenean bibendum enim at ligula ultricies, non dapibus libero aliquet. Vivamus in massa id libero facilisis rutrum. Sed posuere tortor quis pharetra porttitor. Pellentesque bibendum, diam et pretium molestie, dui sapien pellentesque tellus, sit amet accumsan elit felis in odio. Nunc pharetra quam erat, vel sodales metus facilisis mattis. Proin cursus ipsum sed egestas semper. Nam pharetra, elit ut sodales suscipit, nulla enim laoreet leo, dictum luctus lacus nisl et lectus. Suspendisse vitae enim purus. Sed eu interdum turpis, vel fermentum mauris. Donec faucibus bibendum magna, eu varius mi dapibus ultrices. Pellentesque ac eleifend ipsum, in tincidunt diam.
 
 Praesent id velit sit amet leo aliquet rhoncus. Nulla vestibulum ante nisl, in ultricies ipsum egestas nec. Suspendisse at feugiat ex. Morbi dictum blandit nisl nec iaculis. Quisque nisi nisl, aliquam et tortor blandit, accumsan lobortis ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur consequat malesuada nunc quis aliquam. Nunc id mauris sed arcu venenatis semper. Sed ut orci eu eros aliquam porta. In lectus nunc, bibendum scelerisque congue et, bibendum et felis. Vivamus semper suscipit gravida. Nullam at tortor elementum, lacinia quam sit amet, congue erat. Morbi a efficitur nisl. Suspendisse tristique tellus vitae aliquet vulputate.
@@ -60,11 +34,11 @@ Quisque egestas elit ligula, in interdum augue congue eget. In sodales dapibus i
 </template>
 
 <style>
-.note {
+.annotation-container {
 	width: 60%;
 }
 
-.note textarea {
+.annotation-container textarea {
 	padding: 50px;
 	width: 80%;
 	font-size: 18px;
