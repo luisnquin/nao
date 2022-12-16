e0e1c94e74ec4f143c46c9e5385c3fe25e95de82
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-16 06:07:32 -0500

    feat: added recover to nao init + errors in init handled by ui pkg 🦭

--- cmd/nao/main.go
+++ cmd/nao/main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/luisnquin/nao/v2/internal/cmd"
 	"github.com/luisnquin/nao/v2/internal/config"
 	"github.com/luisnquin/nao/v2/internal/data"
+	"github.com/luisnquin/nao/v2/internal/ui"
 )
 
 func main() {
+	defer func() {
+		if v := recover(); v != nil {
+			ui.Fatalf("%v", v)
+			os.Exit(1)
+		}
+	}()
+
 	config, err := config.New()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err.Error())
+		ui.Error(err.Error())
 		os.Exit(1)
 	}
 
 	data, err := data.NewBuffer(config)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err.Error())
+		ui.Error(err.Error())
 		os.Exit(1)
 	}
 
 	if err := cmd.Execute(config, data); err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err.Error())
+		ui.Error(err.Error())
 		os.Exit(1)
 	}
 }
