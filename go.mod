fa138e6af49c179358bd4fad50c5e4d48e1fba45
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-18 23:30:17 -0500

    feat(version): now version shows the binary status relative to the latest github tag

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/luisnquin/nao/v3
 go 1.18
 
 require (
+	github.com/Masterminds/semver/v3 v3.2.0
 	github.com/ProtonMail/go-appdir v1.1.0
+	github.com/charmbracelet/bubbletea v0.23.1
 	github.com/cip8/autoname v1.0.1
 	github.com/enescakir/emoji v1.0.0
 	github.com/goccy/go-json v0.9.11
@@ -17,17 +19,28 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/go-openapi/errors v0.19.8 // indirect
 	github.com/go-openapi/strfmt v0.21.2 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	go.mongodb.org/mongo-driver v1.7.5 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -1,6 +1,13 @@
 package cmd
 
 import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/goccy/go-json"
+	"github.com/gookit/color"
 	"github.com/luisnquin/nao/v3/internal/config"
 	"github.com/luisnquin/nao/v3/internal/ui"
 	"github.com/spf13/cobra"
@@ -11,6 +18,19 @@ type VersionCmd struct {
 	*cobra.Command
 }
 
+type githubTagInfo struct {
+	NodeID     string            `json:"node_id"`
+	Name       string            `json:"name"`
+	ZipBallUrl string            `json:"zipball_url"`
+	TarBallUrl string            `json:"tarball_url"`
+	Commit     map[string]string `json:"commit"`
+}
+
+const (
+	tagsUrl = "https://api.github.com/repos/luisnquin/nao/tags?per_page=1"
+	version = "v2.2.0"
+)
+
 func BuildVersion(config *config.AppConfig) VersionCmd {
 	return VersionCmd{
 		Command: &cobra.Command{
@@ -18,8 +38,98 @@ func BuildVersion(config *config.AppConfig) VersionCmd {
 			Short: "Print the nao version number",
 			Args:  cobra.NoArgs,
 			Run: func(cmd *cobra.Command, args []string) {
-				ui.GetPrinter(config.Command.Version.Color).Println("nao v2.2.0")
+				client := http.Client{Timeout: time.Second}
+
+				// TODO improve this
+				req, err := http.NewRequestWithContext(cmd.Context(), http.MethodGet, tagsUrl, nil)
+				if err != nil {
+					panic(err)
+				}
+
+				req.Header.Add("Accept", "application/vnd.github+json")
+
+				res, err := client.Do(req)
+				if err != nil {
+					panic(err)
+				}
+
+				tags := make([]*githubTagInfo, 0, 1)
+
+				err = json.NewDecoder(res.Body).Decode(&tags)
+				if err != nil {
+					panic(err)
+				}
+
+				remoteVersion, err := semver.NewVersion(tags[0].Name)
+				if err != nil {
+					panic(err)
+				}
+
+				binaryVersion := semver.MustParse(version)
+
+				var b strings.Builder
+
+				b.WriteString("nao ")
+				b.WriteString(version)
+				b.WriteString(", ")
+
+				_, err = client.Get("http://clients3.google.com/generate_204")
+				if err == nil {
+					if remoteVersion.GreaterThan(binaryVersion) {
+						b.WriteString("outdated ") // ! maybe change color of this
+
+						diff := remoteVersion.Compare(binaryVersion)
+
+						var (
+							diffPrinter color.PrinterFace
+							diffLabel   string
+						)
+
+						switch {
+						case remoteVersion.Major() > binaryVersion.Major():
+							diffPrinter = ui.GetPrinter("#b32d3a") // Red
+							diffLabel = "major"
+
+						case remoteVersion.Minor() > binaryVersion.Minor():
+							diffPrinter = ui.GetPrinter("#ded55b") // Yellow
+							diffLabel = "minor"
+
+						case remoteVersion.Patch() > binaryVersion.Patch():
+							diffPrinter = ui.GetPrinter("#b32d3a") // Gray
+							diffLabel = "patch"
+
+						default:
+							diffPrinter = ui.GetPrinter("#7925c2")
+							diffLabel = "?"
+						}
+
+						if diff > 1 && diffLabel != "?" {
+							diffLabel += "s"
+						}
+
+						b.WriteString(diffPrinter.Sprintf("(↓%d %s)", diff, diffLabel))
+					} else {
+						b.WriteString("everything up-to-date")
+					}
+				} else {
+					b.WriteString("state unknown")
+				}
+
+				ui.GetPrinter(config.Command.Version.Color).Println(b.String())
 			},
 		},
 	}
 }
+
+/*
+patches #545c61
+minors #ded55b
+majors #b32d3a
+*/
+
+// outdated (↓5 majors)
+//  outdated (↓5 fixes)
+
+// up to date ;)
+
+// state unknown
