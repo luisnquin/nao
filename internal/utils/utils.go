ccd327913467923db4d457eee6c8b34c4d0afdd6
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-02-03 23:07:28 -0500

    feat(utils): added FileExists func + more comments

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/agnivade/levenshtein"
 )
 
+// Asks for the hour to the host and returns true in case of consider this a lucky day.
 func Contains[T comparable](slice []T, target T) bool {
 	for _, el := range slice {
 		if el == target {
@@ -16,17 +17,23 @@ func Contains[T comparable](slice []T, target T) bool {
 	return false
 }
 
-func IsDirectory(path string) (bool, error) {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return false, err
-	}
-
-	return fi.IsDir(), nil
-}
-
+// Champion among champions, it came to change your dear world.
 func Ptr[T any](v T) *T {
 	return &v
+}
+
+// Checks that path exists and isn't a directory.
+func FileExists(path string) bool {
+	info, err := os.Stat(path)
+
+	return err == nil && !info.IsDir()
+}
+
+// Checks that path exists and isn't a file.
+func IsDirectory(path string) bool {
+	info, err := os.Stat(path)
+
+	return err == nil && info.IsDir()
 }
 
 // Using the levenshtein distance algorithm, it returns
