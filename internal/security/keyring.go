6e2b2093522ae2c681fb5a6c1b2988d854ac22fc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-02-03 23:02:45 -0500

    feat(security): added delete secret from keyring func

--- internal/security/keyring.go
+++ internal/security/keyring.go
@@ -34,3 +34,17 @@ func SetSecretInKeyring(secret string) error {
 
 	return keyring.Set(internal.AppName, caller.Username, secret)
 }
+
+// Deletes a secret stored in the system keyring. It uses the current
+// username of the caller and the app name to access the secret.
+//
+// If the current username can't be determined, the function panics
+// with an error.
+func DeleteSecretFromKeyring() error {
+	caller, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	return keyring.Delete(internal.AppName, caller.Username)
+}
