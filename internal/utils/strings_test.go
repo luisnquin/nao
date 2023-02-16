b9d23bc1858a3e8057d97860ea2ac241bebfd8b4
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-02-16 18:00:07 -0500

    test(utils): added unit test for stuff to convert text to pascal case

--- internal/utils/strings_test.go
+++ internal/utils/strings_test.go
@@ -6,6 +6,40 @@ import (
 	"github.com/luisnquin/nao/v3/internal/utils"
 )
 
+func TestToPascalCase(t *testing.T) {
+	checks := []struct {
+		in, out string
+	}{
+		{
+			in:  "Hello world",
+			out: "HelloWorld",
+		},
+		{
+			in:  "pascal case",
+			out: "PascalCase",
+		},
+		{
+			in:  "What do you think of the cookies?",
+			out: "WhatDoYouThinkOfTheCookies?",
+		},
+		{
+			in:  "space odissey",
+			out: "SpaceOdissey",
+		},
+		{
+			in:  "you can't see me anymore!",
+			out: "YouCan'tSeeMeAnymore!",
+		},
+	}
+
+	for _, expected := range checks {
+		if out := utils.ToPascalCase(expected.in); out != expected.out {
+			t.Errorf("expected '%s', but got '%s' from '%s'", expected.out, out, expected.in)
+			t.Fail()
+		}
+	}
+}
+
 func TestToCamelCase(t *testing.T) {
 	checks := []struct {
 		in, out string
